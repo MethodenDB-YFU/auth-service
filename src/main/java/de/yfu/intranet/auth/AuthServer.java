c476cfa2d6ca15511c31e39b4ac6d61b82c217ba
Alex Senger <alex@alexmsenger.de>
2018-01-29 12:29:41 +0100

    #1 refactoring

--- src/main/java/de/yfu/intranet/auth/AuthServer.java
+++ src/main/java/de/yfu/intranet/auth/AuthServer.java
@@ -35,26 +35,5 @@ public class AuthServer extends WebMvcConfigurerAdapter {
       -d 'scope.read=true&scope.write=true&scope.openid=true'
     */
 
-    @Configuration
-    @EnableAuthorizationServer
-    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
-        @Autowired
-        private AuthenticationManager authenticationManager;
-
-        @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints.authenticationManager(authenticationManager);
-        }
-
-        @Override
-        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-            clients.inMemory()
-                    .withClient("acme")
-                    .secret("acmesecret")
-                    .authorizedGrantTypes("authorization_code", "refresh_token",
-                            "password").scopes("openid");
-        }
-
-    }
 }
